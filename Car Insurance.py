d047bccd9fc51d8097f707d33f51c829def6d41f
Pranavcoding <77463685+Pranavcoding@users.noreply.github.com>
2021-02-18 17:14:07 -0500

    Add files via upload

---
+++ Car Insurance.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name_car = {
+    "Sam" : "Tesla",
+    "Sach" : "Toyota",
+    "Pran" : "Nissan" 
+}
+
+
+# In[5]:
+
+
+print (name_car)
+
+
+# In[59]:
+
+
+ownername = input("")
+
+
+# In[60]:
+
+
+brandname = input("")
+
+
+# In[44]:
+
+
+print (brandname)
+
+
+# In[45]:
+
+
+print (ownername)
+
+
+# In[49]:
+
+
+
+
+
+# In[53]:
+
+
+ownernametwo = input("")
+
+
+# In[54]:
+
+
+brandnametwo = input("")
+
+
+# In[55]:
+
+
+ownernamethree = input("")
+
+
+# In[56]:
+
+
+brandnamethree = input("")
+
+
+# In[61]:
+
+
+print (ownername + " owns a " + brandname)
+print (ownernametwo + " owns a " + brandnametwo)
+print (ownernamethree + " owns a " + brandnamethree)
+
+
+# In[ ]:
+
+
+
+
